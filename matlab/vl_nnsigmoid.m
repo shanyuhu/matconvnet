720a37ae2abc99e35f70ddbddaea0e30e9f96792
shmoda-k <shimoda-k@mm.inf.uec.ac.jp>
2016-10-07 14:43:56 +0900

    '16/10/07'

--- matlab/vl_nnsigmoid.m
+++ matlab/vl_nnsigmoid.m
@@ -1,24 +1,30 @@
-function out = vl_nnsigmoid(x,dzdy)
-%VL_NNSIGMOID CNN sigmoid nonlinear unit.
-%   Y = VL_NNSIGMOID(X) computes the sigmoid of the data X. X can
-%   have an arbitrary size. The sigmoid is defined as follows:
+function Y = vl_nnsigmoid(X,dzdY)
+% VL_NNSOFTMAX  CNN softmax
+%    Y = VL_NNSOFTMAX(X) applies the softmax operator the data X. X
+%    has dimension H x W x D x N, packing N arrays of W x H
+%    D-dimensional vectors.
 %
-%     SIGMOID(X) = 1 / (1 + EXP(-X)).
+%    D can be thought of as the number of possible classes and the
+%    function computes the softmax along the D dimension. Often W=H=1,
+%    but this is not a requirement, as the operator is applied
+%    convolutionally at all spatial locations.
 %
-%   DZDX = VL_NNSIGMOID(X, DZDY) computes the derivative of the
-%   block projected onto DZDY. DZDX and DZDY have the same
-%   dimensions as X and Y respectively.
+%    DZDX = VL_NNSOFTMAX(X, DZDY) computes the derivative DZDX of the
+%    CNN output with respect to the input X given the derivative DZDY
+%    with respect to the block output Y. DZDX has the same dimension
+%    as X.
 
-% Copyright (C) 2015 Karel Lenc.
+% Copyright (C) 2014 Andrea Vedaldi.
 % All rights reserved.
 %
 % This file is part of the VLFeat library and is made available under
 % the terms of the BSD license (see the COPYING file).
 
-y = 1 ./ (1 + exp(-x));
+Y = logsig(X);
 
-if nargin <= 1 || isempty(dzdy)
-  out = y ;
-else
-  out = dzdy .* (y .* (1 - y)) ;
-end
+if nargin <= 1, return ; end
+
+% backward
+Y=single(dzdY);
+%Y = dzdY.*Y.*(ones(size(X))-Y);
+%Y = Y .* bsxfun(@minus, dzdY, sum(dzdY .* Y, 3)) ;
